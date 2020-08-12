de2fbf260f1e11c9bcaddc50df6358d9ab48d44a
Madhu2244 <63474175+Madhu2244@users.noreply.github.com>
2020-08-11 19:45:22 -0500

    Added Pretty Formatting, and Sorting Algorithms

--- GPA_Calculation/src/com/driver/GPA_Calculations.java
+++ GPA_Calculation/src/com/driver/GPA_Calculations.java
@@ -7,15 +7,6 @@ import com.list.CourseList;
 public class GPA_Calculations {
 	public static CourseList myCourseList;
 	
-	public static void main(String args[]) package com.driver;
-
-import java.util.Scanner;
-
-import com.list.CourseList;
-
-public class GPA_Calculations {
-	public static CourseList myCourseList;
-	
 	public static void main(String args[]) 
 	{
 		Scanner scanner = new Scanner(System.in);
@@ -39,7 +30,7 @@ public class GPA_Calculations {
 			if (choice == 2)
 				myCourseList.addCourse(Course_Methods.createCrouse());
 			if (choice == 3)
-				System.out.println(myCourseList.toString());
+				System.out.println(Course_Methods.toStringCourses(myCourseList));
 			if (choice == 4)
 				System.out.println(myCourseList.calculateTotalGPA());
 			if (choice == 5)
@@ -66,16 +57,16 @@ public class GPA_Calculations {
 				int semester1 = Integer.parseInt(scanner.nextLine());
 				System.out.println("Type in the second semester grade");
 				int semester2 = Integer.parseInt(scanner.nextLine());
-				System.out.println(semester1 + "," + semester2 + "You need a: " + Semester.one(semester1, semester2) + " to first tier (97+)");
-				System.out.println(semester1 + "," + semester2 + "You need a: " + Semester.two(semester1, semester2) + " to second tier (93-96)");
-				System.out.println(semester1 + "," + semester2 + "You need a: " + Semester.three(semester1, semester2) + " to third tier (90-92)");
-				System.out.println(semester1 + "," + semester2 + "You need a: " + Semester.four(semester1, semester2) + " to fourth tier (87-99)");
-				System.out.println(semester1 + "," + semester2 + "You need a: " + Semester.five(semester1, semester2) + " to fifth tier (83-86)");
-				System.out.println(semester1 + "," + semester2 + "You need a: " + Semester.six(semester1, semester2) + " to sixth tier (80-82)");
-				System.out.println(semester1 + "," + semester2 + "You need a: " + Semester.seven(semester1, semester2) + " to seventh tier (77-79)");
-				System.out.println(semester1 + "," + semester2 + "You need a: " + Semester.eight(semester1, semester2) + " to eighth tier (73-76)");
-				System.out.println(semester1 + "," + semester2 + "You need a: " + Semester.nine(semester1, semester2) + " to ninth tier (71-72)");
-				System.out.println(semester1 + "," + semester2 + "You need a: " + Semester.ten(semester1, semester2) + " to pass (70)");
+				System.out.println(semester1 + "," + semester2 + " You need a: " + Semester.one(semester1, semester2) + " to first tier (97+)");
+				System.out.println(semester1 + "," + semester2 + " You need a: " + Semester.two(semester1, semester2) + " to second tier (93-96)");
+				System.out.println(semester1 + "," + semester2 + " You need a: " + Semester.three(semester1, semester2) + " to third tier (90-92)");
+				System.out.println(semester1 + "," + semester2 + " You need a: " + Semester.four(semester1, semester2) + " to fourth tier (87-99)");
+				System.out.println(semester1 + "," + semester2 + " You need a: " + Semester.five(semester1, semester2) + " to fifth tier (83-86)");
+				System.out.println(semester1 + "," + semester2 + " You need a: " + Semester.six(semester1, semester2) + " to sixth tier (80-82)");
+				System.out.println(semester1 + "," + semester2 + " You need a: " + Semester.seven(semester1, semester2) + " to seventh tier (77-79)");
+				System.out.println(semester1 + "," + semester2 + " You need a: " + Semester.eight(semester1, semester2) + " to eighth tier (73-76)");
+				System.out.println(semester1 + "," + semester2 + " You need a: " + Semester.nine(semester1, semester2) + " to ninth tier (71-72)");
+				System.out.println(semester1 + "," + semester2 + " You need a: " + Semester.ten(semester1, semester2) + " to pass (70)");
 			}
 			if (choice == 9)
 				break;
